523b8d678183e4c8a7448e9a9ccbe742f934a01e
JuanGalarza <groupfuzion@gmail.com>
2022-07-18 01:02:21 -0300

    Desafio 3

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+  <div className="nav-container">
+    <div className="nav-logo">Logo!</div>
+    <div className="nav-right">
+    <button>{zapas.cardsName}</button>
+    </div>
+  </div>
+  );
+};
+
+export default Navbar;
---
+++ components/cards.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Cards({ cardsImage, cardsName, cardsDesc, handleClick }) {
+  return (
+    <div className="cards">
+      <img className="cardsImg" src={cardsImage} alt="" />
+      <div className="cardsContent">
+        <h3>{cardsName}</h3>
+        <p>{cardsDesc}</p>
+        <button onClick={() => handleClick(cardsName)}>Comprar</button>
+        </div>
+    </div>
+  );
+}
+
+export default Cards;
