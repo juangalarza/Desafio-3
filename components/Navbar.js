48faeb0f2a55133f122226fb9d8c4542b6d14097
Juan Galarza <98394325+juangalarza@users.noreply.github.com>
2022-07-26 19:23:06 -0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,14 +5,14 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 
 export default function ButtonAppBar({ cardsName }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          /*<IconButton
             size="large"
             edge="start"
             color="inherit"
@@ -20,7 +20,7 @@ export default function ButtonAppBar({ cardsName }) {
             sx={{ mr: 2 }}
           >
             <MenuIcon />
-          </IconButton>
+          </IconButton>*/
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Zapas Nike
           </Typography>
