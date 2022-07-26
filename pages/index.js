8d5b3558cd6279933cfd07c4ebaf296545981b35
Juan Galarza <98394325+juangalarza@users.noreply.github.com>
2022-07-26 20:12:49 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,9 @@ function Home() {
       
       <Grid container spacing={2} p="20px 24px">
         {products.map((datos, id) => (
-          <div key={id}>
+          
           <Grid item xs={12} sm={6} md={3} xl={3}>
+          <div key={id}>
           <Card 
             cardsName={datos.cardsName}
             cardsDesc={datos.cardsDesc}
@@ -41,8 +42,9 @@ function Home() {
 
             handleClick={handleClick}
           />
+              </div>
           </Grid>
-        </div>
+        
         ))}
         </Grid>
     
