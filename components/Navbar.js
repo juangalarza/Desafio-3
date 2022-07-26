d20c14ed9a72313b56ab3d77eaa7798f440ba913
Juan Galarza <98394325+juangalarza@users.noreply.github.com>
2022-07-26 19:24:19 -0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,15 +12,7 @@ export default function ButtonAppBar({ cardsName }) {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          /*<IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>*/
+          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Zapas Nike
           </Typography>
